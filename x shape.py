ad4ad3ee83b26f902b66a5e6dba935772208136e
Vimal MR <78142299+Vimalmr@users.noreply.github.com>
2023-01-31 22:58:59 +0530

    Update x shape.py

--- x shape.py
+++ x shape.py
@@ -1,21 +1,8 @@
 n=int(input("length:"))
-a=int(n/2)+1
-for i in range(1,n+1):
-    if(i<=a):
-        for k in range(1,i):
-            print("  ",end="")
-        for j in range(i,n+1):
-            if(j==i or j==n-i+1):
-                print("* ",end="")
-            else:
-                print("  ",end="")
-        print("")
-    else:
-        for k in range(i,n-1):
-            print("  ",end="")
-        for j in range(1,n+1):
-            if(j==i or j==n-i+1):
-                print("* ",end="")
-            else:
-                print("  ",end="")
-        print("")
+for i in range(1,2*n):
+    for j in range(1,2*n):
+        if (i==j or i+j==2*n):
+            print('* ', end='')
+        else:
+            print('  ', end='')
+    print()
